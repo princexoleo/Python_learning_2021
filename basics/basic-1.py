058fc801d7abf8c0208aa4074b3c61cf47b385a9
Leon <xtleon.xl@gmail.com>
2021-05-20 09:01:20 +0600

    Added the basic thing for python string

--- basics/basic-1.py
+++ basics/basic-1.py
@@ -105,3 +105,6 @@ print("Day -1 String Manupulation")
 print("String Concatenation is done with the '+' sign.")
 print('e.g. print("Hello "+"world")')
 print(("New lines can be created with a backlash and n."))
+
+
+
